4837e10d602ca2aa5e33545b4fd1be5185ea43e0
Michael <mlonnecker@gmail.com>
2019-06-26 18:46:04 -0600

    Extention Methods fun String.startsWith() : ... infix fun Int.multiplyBy(mult: Int) = this * mult ...

--- src/Practice.kt
+++ src/Practice.kt
@@ -12,9 +12,26 @@ object StringUtils {
     fun startsWithUpperCase(value: String?) : Boolean = !(value.isNullOrBlank()) && value!![0].isUpperCase()
 }
 
+// extension method. used in place of decorator pattern.
+fun String?.startsWithUpperCase() : Boolean = !(this.isNullOrBlank()) && this!![0].isUpperCase()
+
+//fun Int.multiplyBy(multiplier: Int) = this * multiplier
+// if single value parameter then using the keyword infix is nice
+infix fun Int.multiplyBy(multiplier: Int) = this * multiplier
+
 fun main(args: Array<String>) {
 
     val myString = "Kotlin"
 
     println("toLower= ${myString.toLowerCase()}")
+
+    println("startsWithUpper = ${StringUtils.startsWithUpperCase(myString)}")
+    println("startsWithUpper = ${StringUtils.startsWithUpperCase(null)}")
+    println("startsWithUpper = ${StringUtils.startsWithUpperCase("")}")
+    println("startsWithUpper = ${StringUtils.startsWithUpperCase("kotlin")}")
+
+    println("startsWithUpper extension method = ${myString.startsWithUpperCase()}")
+
+    println("multiplyBy = ${5.multiplyBy(3)}")
+    println("multiplyBy = ${5 multiplyBy 5}") //with infix keyword used.
 }
