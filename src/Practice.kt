ca207d1a7d86ed3619d1b5fe370e3a4b61bd1de5
Michael <mlonnecker@gmail.com>
2019-06-26 18:00:48 -0600

    fun startsWithUpperCase (value:String?) :Boolean = !(value.isNullOrBlank()) && value??.[0].isUpperCase()

--- src/Practice.kt
+++ src/Practice.kt
@@ -1,14 +1,15 @@
 
 object StringUtils {
+/*
     fun startsWithUpperCase(value: String?): Boolean {
-/*        if (value.isNullOrBlank()) {
+        if (value.isNullOrBlank()) {
             return false
         }
         return value!![0].isUpperCase()
-*/
         return !(value.isNullOrBlank()) && value!![0].isUpperCase()
     }
-
+*/
+    fun startsWithUpperCase(value: String?) : Boolean = !(value.isNullOrBlank()) && value!![0].isUpperCase()
 }
 
 fun main(args: Array<String>) {
