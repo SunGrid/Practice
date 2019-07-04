ce2016059fe4057b5f1a7e843d18c5263ea8e8c3
Michael <mlonnecker@gmail.com>
2019-07-04 14:12:09 -0600

    Overload minus and times operators. Complex number with formula

--- src/Practice.kt
+++ src/Practice.kt
@@ -5,8 +5,27 @@ Override toString
 
 class ComplexNumber( val real: Float, val imaginary: Float) {
 
-    operator fun plus(other: ComplexNumber): ComplexNumber {
+    operator fun plus(other: ComplexNumber) : ComplexNumber {
         return ComplexNumber(real + other.real, imaginary + other.imaginary)
+    }
+
+    operator fun minus(other: ComplexNumber) : ComplexNumber {
+        return ComplexNumber(real - other.real, imaginary - other.imaginary)
+    }
+
+    operator fun times(other: ComplexNumber) : ComplexNumber {
+//   (a + bi) * (c + di) = (ac - bd) + (bc + ad)i
+//   (2 + 2i) * (3 - 5i)
+// = (2 * 3 - 2 * (-5)) + (2 * 3 + 2 * (-5))i
+// = (6 - (-10)) + (6 + (-10))i
+// = (6 + 10) + (6 - 10 )i
+// = 16 - 4i
+        //                         (ac           -           bd)
+        val realResult = real * other.real - imaginary * other.imaginary
+        //                                   (bc           +      ad)
+        val imaginaryResult = imaginary * other.real + real * other.imaginary
+
+        return ComplexNumber(realResult, imaginaryResult)
     }
 
     override fun toString(): String {
@@ -29,6 +48,9 @@ fun main(){
 
     val sum = first + second
 
+
     println("sum = $sum")
+    println("first-second = ${first - second}")
+    println("first*second = ${first * second}")
 
 }
