f9666a583ae0994dc085dadffab0fed5bd47d13b
Michael <mlonnecker@gmail.com>
2019-07-03 23:00:18 -0600

    Overloading Operators compx nums, Override toString

--- src/Practice.kt
+++ src/Practice.kt
@@ -1,115 +1,34 @@
-/* Inheritance and Polymorphism
+/*
+overloading operators and complex numbers.
+Override toString
+*/
 
- */
-open class Enemy(health: Int, var weapon: String) {
+class ComplexNumber( val real: Float, val imaginary: Float) {
 
-    var health: Int = 0
-        set(value){
-            field = value
+    operator fun plus(other: ComplexNumber): ComplexNumber {
+        return ComplexNumber(real + other.real, imaginary + other.imaginary)
+    }
 
-            if (value < 0){
-                field = 0
-            }
+    override fun toString(): String {
+
+        var sign = "+"
+
+        if (imaginary < 0){
+            sign = "-"
         }
-    var damage: Int = 0
-
-    init {
-        //without this.health = health, passed from mian method, the var health up above will be set to 0 and run at 0
-        this.health = health
-
-        println("Enemy inint called")
-    }
-
-    //polymorphic parameter
-    open fun attack(enemy: Enemy) {
-        println("attacking $enemy with $weapon")
-        enemy.takeDamage(damage)
-    }
-
-    fun takeDamage(damageToTake: Int) {
-        health -= damageToTake
-    }
-
-}
-
-class Pikeman(health: Int, var armor: Int) : Enemy(health, "pike") {
-
-    init {
-        println("Pikeman init called")
+        return "$real $sign ${Math.abs(imaginary)}i"
     }
 }
 
-class Archer(health: Int, var arrowCount: Int) : Enemy(health, "bow") {
+fun main(){
+    val first = ComplexNumber (2f, 2f)
+    val second = ComplexNumber(3f, -5f)
 
-    init {
-        println("Archer init called")
-    }
+    println("first = $first")
+    println("second = $second")
 
-    override fun attack(enemy: Enemy) {
-        if (arrowCount <= 0) {
-            println("no more arrows")
-        } else {
-            super.attack(enemy)
-            arrowCount--
-            println("arrows left = $arrowCount")
-        }
-    }
-}
+    val sum = first + second
 
-class Pistolero(health: Int, var bulletCount: Int = 0) : Enemy(health,"pistol") {
-
-
-    fun reload(){
-        if(bulletCount <=0){
-            println("reloading pistol")
-            bulletCount = 6
-        }
-    }
-    init {
-        println("Pistolero init called")
-    }
-
-    override fun attack(enemy: Enemy) {
-        if (bulletCount <= 0){
-            println("No more bullets. Reload!")
-            reload()
-        }
-        super.attack(enemy)
-        println("firing bullet at $enemy")
-        bulletCount --
-        println("bullets left = $bulletCount")
-
-
-    }
-}
-
-fun main() {
-
-    //vals being declared as "Enemy"
-    val pikeman: Enemy = Pikeman(100, 100)
-    pikeman.damage = 5
-
-    val archer: Enemy = Archer(health = 100, arrowCount = 5)
-    archer.damage = 5
-
-    val pistolero: Enemy = Pistolero(100,6)
-    pistolero.damage = 10
-
-    while (archer.health > 0){
-        pistolero.attack(archer)
-    }
-    println("archer died")
-
-/*    pikeman.attack(archer)
-    println("pikeman health = ${pikeman.health} archer health = ${archer.health}")
-
-    archer.attack(pikeman)
-    println("pikeman health = ${pikeman.health} archer health = ${archer.health}")
-
-    pikeman.attack(archer)
-    println("pikeman health = ${pikeman.health} archer health = ${archer.health}")
-
-    archer.attack(pikeman)
-    println("pikeman health = ${pikeman.health} archer health = ${archer.health}")*/
+    println("sum = $sum")
 
 }
